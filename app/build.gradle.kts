c0de4a029601bafd06ae2f6f7529f6c62ba30e94
leonardbantoro <leonardbantoro08@gmail.com>
2025-11-02 13:32:10 +0700

    Commit No. 1 – add first & second worker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.work.runtime)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
