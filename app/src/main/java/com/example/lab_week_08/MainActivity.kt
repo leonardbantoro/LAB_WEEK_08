2822baba8ab3623ff0af518c4549f4c2dad74246
leonardbantoro <leonardbantoro08@gmail.com>
2025-11-02 14:09:26 +0700

    Commit No. 2 – add first foreground service

--- app/src/main/java/com/example/lab_week_08/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_08/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.lab_week_08
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.work.Constraints
@@ -26,10 +30,22 @@ class MainActivity : AppCompatActivity() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v,
                                                                              insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right,
-                systemBars.bottom)
+            v.setPadding(
+                systemBars.left, systemBars.top, systemBars.right,
+                systemBars.bottom
+            )
             insets
         }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) !=
+                PackageManager.PERMISSION_GRANTED
+            ) {
+
+                requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1)
+            }
+        }
+
+
         //Create a constraint of which your workers are bound to.
         //Here the workers cannot execute the given process if
         //there's no internet connection
@@ -47,16 +63,21 @@ class MainActivity : AppCompatActivity() {
         val firstRequest = OneTimeWorkRequest
             .Builder(FirstWorker::class.java)
             .setConstraints(networkConstraints)
-            .setInputData(getIdInputData(
-                FirstWorker
-                .INPUT_DATA_ID, id)
+            .setInputData(
+                getIdInputData(
+                    FirstWorker
+                        .INPUT_DATA_ID, id
+                )
             ).build()
         //This request is created for the SecondWorker class
         val secondRequest = OneTimeWorkRequest
             .Builder(SecondWorker::class.java)
             .setConstraints(networkConstraints)
-            .setInputData(getIdInputData(SecondWorker
-                .INPUT_DATA_ID, id)
+            .setInputData(
+                getIdInputData(
+                    SecondWorker
+                        .INPUT_DATA_ID, id
+                )
             ).build()
         //Sets up the process sequence from the work manager instance
         //Here it starts with FirstWorker, then SecondWorker
@@ -84,16 +105,47 @@ class MainActivity : AppCompatActivity() {
             .observe(this) { info ->
                 if (info.state.isFinished) {
                     showResult("Second process is done")
+                    launchNotificationService()
                 }
             }
     }
+
     //Build the data into the correct format before passing it to the worker as input
     private fun getIdInputData(idKey: String, idValue: String) =
         Data.Builder()
             .putString(idKey, idValue)
             .build()
+
     //Show the result as toast
     private fun showResult(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
+
+    //Launch the NotificationService
+    private fun launchNotificationService() {
+        //Observe if the service process is done or not
+        //If it is, show a toast with the channel ID in it
+        NotificationService.trackingCompletion.observe(
+            this
+        ) { Id ->
+            showResult("Process for Notification Channel ID $Id is done!")
+        }
+
+        //Create an Intent to start the NotificationService
+        //An ID of "001" is also passed as the notification channel ID
+        val serviceIntent = Intent(
+            this,
+            NotificationService::class.java
+        ).apply {
+            putExtra(EXTRA_ID, "001")
+        }
+
+        //Start the foreground service through the Service Intent
+        ContextCompat.startForegroundService(this, serviceIntent)
+    }
+
+    companion object {
+        const val EXTRA_ID = "Id"
+    }
 }
+
